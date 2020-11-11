dd5f2ed8b0cad86159d0dcee32d08c278616b611
maksim2302 <maximdracev95@gmail.com>
2020-11-11 13:17:05 +0400

    Initial Commit

---
+++ FriendshipTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FriendshipTest
+//
+//  Created by 1111 on 11.11.2020.
+//  Copyright © 2020 1111. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
